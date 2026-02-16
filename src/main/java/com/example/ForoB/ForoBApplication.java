92b88bad8258ea6e9d99668d8c29a01ce3c9f90c
jado21 <jado21@outlook.com>
2026-02-16 10:50:29 -0500

    Adding spring

---
+++ src/main/java/com/example/ForoB/ForoBApplication.java
@@ -0,0 +1,13 @@
+package com.example.ForoB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ForoB/ForoBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ForoB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
